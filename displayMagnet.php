00cac1c9f801e00ef03bc160533def5267db8f1a
sansuiKID <38695183+sansuiKID@users.noreply.github.com>
2018-04-26 00:17:57 +0800

    Create displayMagnet.php
    根据id获取该文章页的torrent文件并将其转换为magnet后显示出来

---
+++ displayMagnet.php
@@ -0,0 +1,12 @@
+<?php
+include './BEncode.php';
+include './BDecode.php';
+include './mainFunc.php';
+$site = 'http://thz2.com';
+$id = empty($_GET['id'])?'1447816':$_GET['id'];
+$torrent = curlGet($site.pregTor(curlGet("$site/thread-$id-1-1.html")));
+$desc = BDecode($torrent);
+$info = $desc['info'];
+$hash = strtoupper(sha1( BEncode($info) ));
+echo sprintf('magnet:?xt=urn:btih:%s&dn=%s', $hash, $info['name']);
+?>
