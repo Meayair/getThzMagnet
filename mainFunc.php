b9937867dd64f467db6aaa02ef677a4a1c2dbcf5
sansuiKID <38695183+sansuiKID@users.noreply.github.com>
2018-04-26 00:09:41 +0800

    Create mainFunc.php
    暂时只有curl函数和正则获取torrent地址两个函数

---
+++ mainFunc.php
@@ -0,0 +1,19 @@
+<?php
+function curlGet($url) 
+{
+    $header[]= 'user-agent:Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36';  
+    $curl = curl_init(); 
+    curl_setopt($curl, CURLOPT_URL, $url); 
+    curl_setopt($curl, CURLOPT_REFERER, $url);
+    curl_setopt($curl, CURLOPT_HTTPHEADER,$header); 
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); 
+    return curl_exec($curl); 
+    curl_close($curl); 
+}
+function pregTor($content)
+{
+    $preg = '/href="imc_attachad-ad.html\\?aid=(\w+)"/is';
+    preg_match($preg,$content,$match);
+    return $tor='http://thz2.com/forum.php?mod=attachment&aid='.$match[1];
+}
+?>
