9f4bc598fe38b163315fedd76256b45096d82015
sansuiKID <38695183+sansuiKID@users.noreply.github.com>
2018-05-16 14:22:03 +0800

    Update mainFunc.php

--- mainFunc.php
+++ mainFunc.php
@@ -1,4 +1,5 @@
 <?php
+$site = 'http://taohuabt.cc';
 function curlGet($url) 
 {
     $header[]= 'user-agent:Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36';  
